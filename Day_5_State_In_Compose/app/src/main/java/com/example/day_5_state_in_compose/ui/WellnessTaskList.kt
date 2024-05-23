bb467ab3860bc7b554fa721f9c97708cef9cfe2d
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-05-23 18:55:33 +0500

    day 5 is completed

---
+++ Day_5_State_In_Compose/app/src/main/java/com/example/day_5_state_in_compose/ui/WellnessTaskList.kt
@@ -0,0 +1,53 @@
+package com.example.day_5_state_in_compose.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessTaskItem(taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier){
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = onClose,
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun WellnessTasksList(
+    list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = list,
+            /**
+             * Use key param to define unique keys representing the items in a mutable list,
+             * instead of using the default key (list position). This prevents unnecessary
+             * recompositions.
+             */
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) }
+            )
+        }
+    }
+}
