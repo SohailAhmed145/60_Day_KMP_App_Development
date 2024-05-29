4e6a98e5c2c5d104f2bc921267602097adcad593
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-05-29 08:13:49 +0500

    WhyNote App Project

---
+++ Day_6-15_whyNotesApp/WhyNote/app/src/main/java/com/example/whynote/notes/presentations/notes_screen/NotesScreen.kt
@@ -0,0 +1,8 @@
+package com.example.whynote.notes.presentations.notes_screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NotesScreen(){
+
+}
---
+++ Day_6-15_whyNotesApp/WhyNote/app/src/main/java/com/example/whynote/notes/presentations/util/components/MyTopAppBar.kt
@@ -0,0 +1,8 @@
+package com.example.whynote.notes.presentations.util.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyTopAppBar(){
+    
+}
